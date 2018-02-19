dbfb267b3cd67331e602f24f5fa3a6f17220340a
micahlee <micah@micahlee.com>
2018-02-19 16:53:27 -0500

    Updating dependencies and clearing the first test suite

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,13 +1,8 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
-  end
-
   test "should get failure" do
-    get sessions_failure_url
+    get '/auth/failure'
     assert_response :success
   end
 
