4a069776d738d9a6329d83e74aa8d047456ff7bc
micahlee <micah@micahlee.com>
2018-02-19 16:57:48 -0500

    Pruning unnecessary tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get failure" do
-    get '/auth/failure'
-    assert_response :success
-  end
-
 end
