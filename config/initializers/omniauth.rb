c58d9e92179fd78408b28f0f2e24161de3f153f0
Micah Lee <micah@micahlee.com>
2021-09-09 06:37:29 -0400

    Allow GET request for omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,5 @@ require File.expand_path('lib/omniauth/strategies/sso', Rails.root)
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :sso, ENV['gtbox_account_client_id'], ENV['gtbox_account_secret']
 end
+
+OmniAuth.config.allowed_request_methods = [:get, :post]
